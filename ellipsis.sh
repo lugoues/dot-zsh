4c51093daca848795a885c4f338b03c9a1c62a6e
Peter Brunner <pbrunner@gmail.com>
2016-11-06 00:38:05 -0400

    Updated zim support

--- ellipsis.sh
+++ ellipsis.sh
@@ -2,28 +2,12 @@
 #
 # lugoues/zsh ellipsis package
 
-# The following hooks can be defined to customize behavior of your package:
-# pkg.install() {
-#     fs.link_files $PKG_PATH
-# }
-
-# pkg.push() {
-#     git.push
-# }
-
-# pkg.pull() {
-#     git.pull
-# }
-
-# pkg.installed() {
-#     git.status
-# }
-#
-# pkg.status() {
-#     git.diffstat
-# }
-
 pkg.install() {
-  echo "Installing Zim"
+  echo "$(tput setaf 2)Installing Zim.$(tput sgr0)"
   git clone --recursive https://github.com/Eriner/zim.git ${PKG_PATH}/zim
+}
+
+pkg.pull() {
+  echo "$(tput setaf 2)Updating Zim.$(tput sgr0)"
+  zsh ./zim/tools/zim_update
 }
