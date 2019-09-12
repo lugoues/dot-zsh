2681e3d20c49da0077fd0b91372b107c2188e843
Peter Brunner <pbrunner@gmail.com>
2019-09-12 17:20:07 -0400

    cleanup

--- zsh/commands/b.zsh
+++ zsh/commands/b.zsh
@@ -1,10 +1,1 @@
 
-beet(){
-  docker run --rm -it \
-    -e PUID=$(id -u beets) -e PGID=$(getent group beets | cut -d: -f3) \
-    -v /mnt/raid/Bittorrent/Music:/mnt/raid/Bittorrent/Music:ro \
-    -v /mnt/raid/Music:/mnt/raid/Music \
-    -v ~/.config/beets:/config \
-    lugoues/beets "$@"
-}
-
