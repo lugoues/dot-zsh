4639bc787d0eeaaa8fa097b9e43c62181b9e3f88
Peter Brunner <peter@lugoues.net>
2022-03-24 11:58:41 -0400

    Add host to title functionality

---
+++ zsh/configuration/92-title.zsh
@@ -0,0 +1,17 @@
+# $1 = type; 0 - both, 1 - tab, 2 - title
+# rest = text
+setTerminalText () {
+    # echo works in bash & zsh
+    local mode=$1 ; shift
+    echo -ne "\033]$mode;$@\007"
+}
+stt_both  () { setTerminalText 0 $@; }
+stt_tab   () { setTerminalText 1 $@; }
+stt_title () { setTerminalText 2 $@; }
+set-window-title() {
+  stt_both "${HOST}"
+}
+
+PR_TITLEBAR=''
+set-window-title
+add-zsh-hook precmd set-window-title
