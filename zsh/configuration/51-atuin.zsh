3c169dc82762ed1208dcb52e3593e9b36bc20aea
Peter Brunner <peter@lugoues.net>
2024-02-23 13:59:31 -0500

    disable setting ZSH_AUTOSUGGEST_STRATEGY

--- zsh/configuration/51-atuin.zsh
+++ zsh/configuration/51-atuin.zsh
@@ -6,5 +6,5 @@ if [ $commands[atuin] ]; then
    _zsh_autosuggest_strategy_atuin_top() {
        suggestion=$(atuin search --cmd-only --limit 1 --search-mode prefix $1)
    }
-   export ZSH_AUTOSUGGEST_STRATEGY=atuin_top
+#   export ZSH_AUTOSUGGEST_STRATEGY=atuin_top
 fi
